e7d13ab2df80214d8783a37f6a78bba8682b92c5
juteng <juteng2005@gmail.com>
2017-04-22 00:37:53 +0900

    add bat file

---
+++ mklink.bat
@@ -0,0 +1,4 @@
+mklink %HOMEPATH%"\.vimrc" %HOMEPATH%"\dotfiles\.vimrc"
+mklink /D %HOMEPATH%"\.vim" %HOMEPATH%"\dotfiles\.vim"
+mklink /D %HOMEPATH%"\.ideavimrc" %HOMEPATH%"\dotfiles\.ideavimrc"
+exit 0
