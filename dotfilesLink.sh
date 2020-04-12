1dcdbb5edcf8a86a6526d884484ced4fdb17dcc0
nochi0105 <wenhua4438@gmail.com>
2020-04-12 23:53:14 +0900

    fix: auto installer for oh-my-zsh

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,4 +1,32 @@
 #! /bin/bash
+apt-get -v &> /dev/null && OS="ubuntu" || OS="centos"
+if [ $OS = "ubuntu"  ]
+then
+	sudo apt update
+	sudo apt install -y git gcc
+
+	# zsh installation
+	sudo apt install -y zsh curl powerline fonts-powerline 
+
+	# install oh-my-zsh
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+	# set default shell to zsh
+	chsh -s /bin/zsh
+
+	# install thefuck
+	sudo apt install -y python3-dev python3-pip python3-setuptools
+	sudo pip3 install thefuck
+else
+	sudo yum -y git gcc
+fi
+
+# import git module
+git pull && git submodule init && git submodule update && git submodule status
+
+# move custom library
+cp -af ~/dotfiles/.vim/userautoload/config/* ~/dotfiles/.vim/bundle/ 
+
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 #ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
@@ -11,25 +39,3 @@ ln -sf ~/dotfiles/bat ~/bat
 ln -sf ~/dotfiles/bin ~/bin
 ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
-
-#git サブモジュール導入
-apt-get -v &> /dev/null && OS="ubuntu" || OS="centos"
-if [ $OS = "ubuntu"  ]
-then
-	sudo apt update
-	sudo apt install -y git gcc
-
-	# zsh installation
-	sudo apt install -y zsh powerline fonts-powerline 
-	# set default shell to zsh
-	chsh -s /bin/zsh
-	# install thefuck
-	sudo apt install -y python3-dev python3-pip python3-setuptools
-	sudo pip3 install thefuck
-else
-	sudo yum -y git gcc
-fi
-git pull && git submodule init && git submodule update && git submodule status
-
-#カスタマイズライブラリの適用
-cp -af ~/dotfiles/.vim/userautoload/config/* ~/dotfiles/.vim/bundle/ 
