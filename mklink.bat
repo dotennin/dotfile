8cfd3c28fd7b832521d9374c62ec75083e2294b6
nochi0105 <wenhua4438@gmail.com>
2020-05-09 22:23:47 +0900

    added custom theme for git bash

--- mklink.bat
+++ mklink.bat
@@ -1,4 +1,5 @@
 mklink %HOMEPATH%"\.vimrc" %HOMEPATH%"\dotfiles\.vimrc"
 mklink /D %HOMEPATH%"\.vim" %HOMEPATH%"\dotfiles\.vim"
 mklink /D %HOMEPATH%"\.ideavimrc" %HOMEPATH%"\dotfiles\.ideavimrc"
+mklink /D %HOMEPATH%"\.minttyrc" %HOMEPATH%"\dotfiles\.minttyrc"
 exit 0
